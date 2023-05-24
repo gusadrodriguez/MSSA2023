735a24dc59aa6a2c9ea2b664777574361f0a2b64
Gustavo Rodriguez <gustavo.a.rodriguez1@outlook.com>
2023-05-24 16:24:11 -0700

    Comitting remaining files

--- IntroToPS_1.ps1
+++ IntroToPS_1.ps1
@@ -1,7 +1,10 @@
-﻿Get-Process -Name msedge
+﻿Get-Process -Name msedge | Get-Service *
 Get-ChildItem e:\ -Name 'm*'
 
 Copy-Item E:\repolanding\MSSA2023\fileInBranch.txt c:\
 
 Get-Help about_Comparison_Operators -ShowWindow
 Get-Help about_Do -ShowWindow
+
+Get-Process * | Get-Member -Name ProcessName
+Get-Service  | Get-Member
