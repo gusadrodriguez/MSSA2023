947f81fe1738ad41bf6728f7ba6c00ab2bc54db6
Gustavo Rodriguez <gustavo.a.rodriguez1@outlook.com>
2023-05-24 21:24:55 -0700

    added string of code

--- setupGitAutomatically.ps1
+++ setupGitAutomatically.ps1
@@ -1,4 +1,7 @@
 ﻿Install-Module -Name PSClassToolsBD09 -Force
 Get-Command -Module PSClassToolBD09 
 
-Install-VSCodeAndGit -GitHubRepoUrl https://github.com/gusadrodriguez/MSSA2023.git -GitFullName "gusadrodriguez" -GitEmailAddress "gustavo.a.rodriguez1@outlook.com"
+Install-VSCodeAndGit -GitHubRepoUrl https://github.com/gusadrodriguez/MSSA2023.git -GitFullName "Gustavo Rodriguez" -GitEmailAddress "gustavo.a.rodriguez1@outlook.com" 
+#all files are now saved under GitRoot folder - DO NOT CHANGE ; username is also full name not alias for GitHub
+
+#add if not found using folder on Visual Studio Code
