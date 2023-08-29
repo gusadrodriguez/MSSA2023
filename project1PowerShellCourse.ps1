2df28d6b38e61935cf144436ab9028650edd3d8b
gusadrodriguez <134262242+gusadrodriguez@users.noreply.github.com>
2023-08-28 23:32:21 -0700

    Update project1PowerShellCourse.ps1
    updates to task 1, and task 2, created plan for task 3 & 4 in comments

--- project1PowerShellCourse.ps1
+++ project1PowerShellCourse.ps1
@@ -2,7 +2,7 @@ $csvFilePath = "E:\NewHires.CSV"
 
 $csvFile = Import-Csv -path $csvFilePath -Delimiter ","
 
-$departments = $csvFile | Select-Object -Property department
+$departments = $csvFile | Select-Object -Property department -Unique
 $departmentsArray = $departments.department
 
 $orgUnits = Get-ADOrganizationalUnit -filter * | Select-Object -property Name
@@ -23,3 +23,15 @@ foreach ($item in $removeOUObjectsArray){
     Get-ADOrganizationalUnit -filter { name -like $item}| Set-ADObject -ProtectedFromAccidentalDeletion:$false -PassThru| Remove-ADOrganizationalUnit -Confirm:$false
     }
 
+
+#Finish creating missing OUs
+
+#Using the CSV we have to add users into Security Groups related to their Department
+
+<#
+For loop through the CSV and take the firstname, lastname, department and save into variable
+for the department variable save the firstname,lastname into ADD-ADGroupMember
+Get-ADGroupMember from OU's to check if the code was successful
+#>
+
+#figure out how to set user account passwords in powershell
